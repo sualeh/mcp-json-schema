e3af2f68218818749c9f116ee9812a92c1cba13b
Sualeh Fatehi <sualeh@hotmail.com>
2025-11-18 08:12:37 -0500

    Update copyright

--- src/main/java/us/fatehi/mcp_json_schema/JsonSchemaGenerator.java
+++ src/main/java/us/fatehi/mcp_json_schema/JsonSchemaGenerator.java
@@ -1,6 +1,6 @@
 /*
  * MCP JSON Schema
- * Copyright (c) 2000-2025, Sualeh Fatehi <sualeh@hotmail.com>.
+ * Copyright (c) 2025, Sualeh Fatehi <sualeh@hotmail.com>.
  * All rights reserved.
  * SPDX-License-Identifier: CC-BY-NC-4.0
  */

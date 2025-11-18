6dcea46d235f212ea503927296135fe456818ba7
Sualeh Fatehi <sualeh@hotmail.com>
2025-11-18 12:59:39 -0500

    Add tests

--- src/test/java/us/fatehi/model_json_schema/JsonSchemaTest.java
+++ src/test/java/us/fatehi/model_json_schema/JsonSchemaTest.java
@@ -1,5 +1,7 @@
 package us.fatehi.model_json_schema;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 
 public class JsonSchemaTest {
@@ -8,6 +10,11 @@ public class JsonSchemaTest {
   public void testSimpleParameters() throws Exception {
     final String schemaJson =
         JsonSchemaGenerator.generateSchema(SampleParameters.class).toPrettyString();
-    System.out.println(schemaJson);
+
+    final String expectedJson = new String(
+        getClass().getResourceAsStream("/expected-schema-simple-parameters.json").readAllBytes(),
+        StandardCharsets.UTF_8);
+
+    assertEquals(expectedJson.strip(), schemaJson.strip());
   }
 }
